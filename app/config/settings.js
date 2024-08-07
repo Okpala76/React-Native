322323918ca602fda4699ebe8a9c7290f469f514
Okpala76 <okpalaogochukwu76@gmail.com>
2024-08-07 19:14:41 +0100

    7/08

--- app/config/settings.js
+++ app/config/settings.js
@@ -2,13 +2,13 @@ import Constants from 'expo-constants';
 
 const settings = { 
     dev: {
-        apiUrl: "http://192.168.83.177:9000/api"
+        apiUrl: "http://192.168.121.45:9000/api"
     },
     staging: {
-        apiUrl: "http://192.168.83.177:9000/api"
+        apiUrl: "http://192.168.121.45:9000/api"
     },
     prod: {
-        apiUrl: "http://192.168.83.177:9000/api"
+        apiUrl: "http://192.168.121.45:9000/api"
     },
 }
 
--- types/react-native-web.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'react-native-web' {
-    import * as ReactNative from 'react-native';
-    export * from 'react-native';
-  }
